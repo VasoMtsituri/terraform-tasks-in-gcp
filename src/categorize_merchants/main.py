f3d359fdff6ed2a1869b5f70e9350d2a18563884
Vaso Mtsituri <vasomtsituri@Vasos-MacBook-Pro.local>
2025-11-02 12:13:07 +0400

    feat(CF): implement and deploy CF to enrich merchants table with categorizations feature

--- src/categorize_merchants/main.py
+++ src/categorize_merchants/main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import pandas_gbq
 from google import genai
 
@@ -8,12 +9,15 @@ GCS_BUCKET = "bog_reports_tmp_gcs_bucket"
 MERCHANTS_TABLE_BQ = f"{PROJECT_ID}.dimensions.dim_merchants"
 GEMINI_API_KEY = "gemini-api-key"
 GEMINI_MODEL = "gemini-2.5-flash"
+MERCHANTS_CATEGORIZED_TABLE_BQ = f"{PROJECT_ID}.dimensions.dim_merchants_categorized"
+
 
 def categorize_merchants(request):
-    df = pandas_gbq.read_gbq(query_or_table=MERCHANTS_TABLE_BQ, project_id=PROJECT_ID)
-    df = df.head()
+    df = pandas_gbq.read_gbq(query_or_table=MERCHANTS_TABLE_BQ,
+                             project_id=PROJECT_ID)
 
-    gemini_api_key = read_gcp_secret(project_id=PROJECT_ID, secret_id=GEMINI_API_KEY)
+    gemini_api_key = read_gcp_secret(project_id=PROJECT_ID,
+                                     secret_id=GEMINI_API_KEY)
 
     client = genai.Client(api_key=gemini_api_key)
     results = []
@@ -32,9 +36,8 @@ def categorize_merchants(request):
             **\n\ncafe\n\nrestaurant\n\nbookstore\n\npharmacy\n\nsupermarket
             \n\nbakery\n\ngym\n\nhairdresser\n\nelectronics\n\nmuseum\n\n\n\n**Output Category:**"""
 
-        merchant_category = client.models.generate_content(
-            model=GEMINI_MODEL, contents=gemini_prompt
-        ).text
+        merchant_category = client.models.generate_content(model=GEMINI_MODEL,
+                                                           contents=gemini_prompt).text
 
         id_category_mapping = {
             "merchant_id": merchant_row["id"],
@@ -42,6 +45,14 @@ def categorize_merchants(request):
         }
         results.append(id_category_mapping)
 
-    print(results)
+    # Join results back to dataframe and save to BigQuery
+    results_df = pd.DataFrame(results)
+    final_df = df.merge(results_df, left_on="id", right_on="merchant_id")
+    # Add new columns to final_df to include timestamp
+    final_df["categorized_at"] = pd.Timestamp.now(tz="UTC")
+    pandas_gbq.to_gbq(final_df,
+                      destination_table=MERCHANTS_CATEGORIZED_TABLE_BQ,
+                      project_id=PROJECT_ID, if_exists="replace")
+
 
     return 'OK'
