a671ea48375a90bb96691aad48b6ee4d71b99902
Vaso Mtsituri <vasomtsituri@Vasos-MacBook-Pro.local>
2025-12-04 10:34:52 +0400

    feat(terraform): update terraform script to deploy composer version 3 in GCP

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,19 @@
 provider "google" {
-      project = "central-catcher-471811-s3"
+      project = var.project
       region  = var.region
 }
 
-resource "google_storage_bucket" "function_source_bucket" {
-      name          = "terraform-task-gcs-bucket-eu"
-      location      = var.location
-      force_destroy = true
+resource "google_composer_environment" "example_environment" {
+  name = "example-environment"
+
+  config {
+
+    software_config {
+      image_version = "composer-3-airflow-2.10.5-build.19"
+    }
+
+    node_config {
+      service_account = "custom-service-account-524@central-catcher-471811-s3.iam.gserviceaccount.com"
+    }
+  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,9 @@
+variable "project" {
+  description = "GCP project ID"
+  type        = string
+  default     = "central-catcher-471811-s3"
+}
+
 variable "location" {
   description = "Location of GCP products"
   type        = string
